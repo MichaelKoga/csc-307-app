61c1ef2365fee31259f88d152a2062bf8e9732de
MichaelKoga <koga_michael@outlook.com>
2024-04-07 14:09:29 -0700

    Commiting IE0

--- node_modules/react-frontend/src/main.jsx
+++ node_modules/react-frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// src/main.jsx 
+import React from "react"; 
+import ReactDOMClient from "react-dom/client"; 
+import MyApp from "./MyApp";
+import "./main.css";  
+// Create the container 
+const container = document.getElementById("root");  
+// Create a root 
+const root = ReactDOMClient.createRoot(container);  
+// Initial render: Render an element to the Root 
+root.render(<MyApp />);
