0f163f090e35cbc8caf9c9c4648d6dc696e94e02
MichaelKoga <koga_michael@outlook.com>
2024-04-15 16:49:13 -0700

    Added query for user id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+const findUserById = (id) => 
+	users["users_list"].find((user) => user["id"] === id);
+
 const findUserByName = (name) => {
 	return users["users_list"].filter(
 		(user) => user["name"] === name
@@ -40,14 +43,29 @@ const users = {
 
 app.use(express.json());
 
+app.get("/users/:id", (req, res) => {  
+	const id = req.params["id"];  
+	let result = findUserById(id);  
+	if (result === undefined)  
+	{   
+		res.status(404).send("Resource not found.");  
+	}  
+	else  
+	{   
+		res.send(result);  
+	} 
+});
+
 app.get("/users", (req, res) => {
 	const name = req.query.name;
-	if(name != undefined) {
+	if(name != undefined) 
+	{
 		let result = findUserByName(name);
 		result = { users_list: result };
 		res.send(result);
 	}
-	else {
+	else 
+	{
 		res.send(users);
 	}
 });
